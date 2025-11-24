0b3b69c19e4966aa7cbdbd7a5c59260ee1b5ad2d
Rayyanta <157342270+NgbRey@users.noreply.github.com>
2025-11-24 23:55:06 +0700

    Update Pembayaran.java

--- pbo/app/Pembayaran/Pembayaran.java
+++ pbo/app/Pembayaran/Pembayaran.java
@@ -1,1 +1,15 @@
+package app.pembayaran;
 
+/**
+ * Interface Pembayaran digunakan sebagai standar untuk
+ * berbagai metode pembayaran yang dapat dipilih oleh pelanggan.
+ * Setiap metode harus dapat memproses pembayaran dan mengembalikan
+ * nama metode yang digunakan.
+ */
+public interface Pembayaran {
+    /**
+     * Memproses pembayaran sejumlah nilai tertentu.
+     */
+    boolean prosesPembayaran(double amount);
+    String getNamaMetode();
+}
