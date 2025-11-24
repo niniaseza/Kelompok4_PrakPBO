98ef7c6ff4d8b5dce4c428676d7cf1c38af49d2e
Azira Kania <azira24@mhs.usk.ac.id>
2025-11-24 23:57:42 +0700

    Update KartuKredit.java

--- pbo/app/Pembayaran/KartuKredit.java
+++ pbo/app/Pembayaran/KartuKredit.java
@@ -1,1 +1,39 @@
+package app.pembayaran;
+
+/**
+ * Kelas KartuKredit merupakan implementasi metode pembayaran
+ * menggunakan kartu kredit. Pembayaran hanya disimulasikan.
+ */
+public class KartuKredit implements Pembayaran {
+    private String namaPemilik;
+    private String noKartu;
+
+    /**
+     * Membuat metode pembayaran kartu kredit.
+     */
+    public KartuKredit(String namaPemilik, String noKartu) {
+        this.namaPemilik = namaPemilik;
+        this.noKartu = noKartu;
+    }
+
+    /**
+     * Mensimulasikan proses pembayaran dengan kartu kredit.
+     */
+    @Override
+    public boolean prosesPembayaran(double amount) {
+        System.out.println("Memproses pembayaran kartu kredit " + noKartu + " sebesar " + amount);
+        System.out.println("Pembayaran kartu sukses");
+        return true;
+    }
+
+    /**
+     * Mengembalikan nama metode pembayaran.
+     *
+     * @return string "Kartu Kredit"
+     */
+    @Override
+    public String getNamaMetode() {
+        return "Kartu Kredit";
+    }
+}
 
