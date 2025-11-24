0d60125caaaa364a51e8dee5cdadd5e920c57a2a
Rayyanta <157342270+NgbRey@users.noreply.github.com>
2025-11-24 23:56:25 +0700

    Update QRIS.java

--- pbo/app/Pembayaran/QRIS.java
+++ pbo/app/Pembayaran/QRIS.java
@@ -1,1 +1,38 @@
+package app.pembayaran;
 
+/**
+ * Metode pembayaran QRIS untuk melakukan transaksi non-tunai.
+ * Kelas ini mensimulasikan proses pembayaran dengan QR code
+ * menggunakan merchant DigitalA.
+ */
+public class QRIS implements Pembayaran {
+    private String merchantName;
+    private String qrisId;
+
+    /**
+     * Constructor default yang menetapkan merchant dan ID QRIS
+     * milik toko DigitalA.
+     */
+    public QRIS() {
+        this.merchantName = "DIGITALA STORE";
+        this.qrisId = "QRIS-DIGITALA-001";
+    }
+
+    /**
+     * Mensimulasikan proses pembayaran melalui QRIS.*/
+    @Override
+    public boolean prosesPembayaran(double amount) {
+        System.out.println("Memproses pembayaran QRIS...");
+        System.out.println("Merchant: " + merchantName);
+        System.out.println("QRIS ID: " + qrisId);
+        System.out.println("Amount: Rp " + amount);
+        System.out.println("Silakan scan QR code untuk melakukan pembayaran");
+        System.out.println("Pembayaran QRIS berhasil");
+        return true;
+    }
+
+    @Override
+    public String getNamaMetode() {
+        return "QRIS";
+    }
+}
