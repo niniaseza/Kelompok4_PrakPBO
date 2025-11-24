8b8d63aea3ceda78b20ad711fa411cc1f16fab3e
Azira Kania <azira24@mhs.usk.ac.id>
2025-11-24 23:54:57 +0700

    Update AkunTidakDitemukan.java

--- pbo/app/exceptions/AkunTidakDitemukan.java
+++ pbo/app/exceptions/AkunTidakDitemukan.java
@@ -1,1 +1,10 @@
-
+package app.exceptions;
+/**
+ * Exception khusus untuk menangani ketika akun tidak ditemukan
+ * saat proses login.
+ */
+public class AkunTidakDitemukanException extends Exception {
+    public AkunTidakDitemukanException(String message) {
+        super(message);
+    }
+}
