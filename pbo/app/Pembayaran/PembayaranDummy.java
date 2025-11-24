cf64998fab71a1ed87b9a5bb28a2668d4b8e9969
Rayyanta <157342270+NgbRey@users.noreply.github.com>
2025-11-24 23:55:34 +0700

    Update PembayaranDummy.java

--- pbo/app/Pembayaran/PembayaranDummy.java
+++ pbo/app/Pembayaran/PembayaranDummy.java
@@ -1,1 +1,29 @@
+package app.pembayaran;
 
+/**
+ * Kelas PembayaranDummy digunakan saat melakukan load data transaksi
+ * dari file. Karena metode pembayaran asli tidak bisa direkonstruksi
+ * sepenuhnya, maka dummy ini menjadi placeholder yang tetap memenuhi
+ * interface Pembayaran.
+ */
+public class PembayaranDummy implements Pembayaran {
+    /** Nama metode pembayaran yang direkonstruksi dari file. */
+    private String namaMetode;
+
+    /**
+     * Membuat objek PembayaranDummy dengan nama metode tertentu.
+     */
+    public PembayaranDummy(String namaMetode) {
+        this.namaMetode = namaMetode;
+    }
+
+    @Override
+    public boolean prosesPembayaran(double amount) {
+        return true; 
+    }
+
+    @Override
+    public String getNamaMetode() {
+        return namaMetode;
+    }
+}
