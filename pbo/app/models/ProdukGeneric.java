e735dc6be6174e08bddaa2186bccf19ff924a73c
all <t.al.jumanatul.ali53@gmail.com>
2025-11-25 00:09:57 +0700

    Update ProdukGeneric.java

--- pbo/app/models/ProdukGeneric.java
+++ pbo/app/models/ProdukGeneric.java
@@ -1,1 +1,19 @@
+package app.models;
 
+public class ProdukGeneric extends ProdukElektronik {
+    
+    public ProdukGeneric(String id, String nama, double hargaBeli, double hargaJual, int stok, String deskripsi) {
+        super(id, nama, hargaBeli, hargaJual, stok, deskripsi);
+    }
+
+    @Override
+    public void displaySpesifikasi() {
+        System.out.println("\n=== DETAIL PRODUK ===");
+        System.out.println("ID       : " + getIdProduk());
+        System.out.println("Nama     : " + getNama());
+        System.out.println("Deskripsi: " + getDeskripsi());
+        System.out.println("Harga    : Rp " + String.format("%.0f", getHargaJual()));
+        System.out.println("Stok     : " + getStok());
+        System.out.println("=====================");
+    }
+}
