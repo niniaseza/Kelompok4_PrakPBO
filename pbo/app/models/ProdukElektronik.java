e22ed585b76f54cfaa170ec543a5d5dcc7eaf868
all <t.al.jumanatul.ali53@gmail.com>
2025-11-25 00:21:50 +0700

    Update ProdukElektronik.java

--- pbo/app/models/ProdukElektronik.java
+++ pbo/app/models/ProdukElektronik.java
@@ -1,5 +1,10 @@
 package app.models;
 
+/**
+ * Kelas abstrak yang merepresentasikan produk elektronik di dalam sistem.
+ * Menyimpan informasi dasar seperti ID, nama, harga, stok, dan deskripsi.
+ * Subclass wajib mengimplementasikan metode displaySpesifikasi().
+ */
 public abstract class ProdukElektronik {
     private String idProduk;
     private String nama;
@@ -8,6 +13,9 @@ public abstract class ProdukElektronik {
     private int stok;
     private String deskripsi;
 
+    /**
+     * Konstruktor untuk membuat produk elektronik baru.
+     */
     public ProdukElektronik(String idProduk, String nama, double hargaBeli, double hargaJual, int stok,
             String deskripsi) {
         this.idProduk = idProduk;
@@ -48,14 +56,17 @@ public abstract class ProdukElektronik {
         this.nama = nama;
     }
 
+    /** Mengubah harga beli produk. */
     public void setHargaBeli(double hargaBeli) {
         this.hargaBeli = hargaBeli;
-    } // ← TAMBAH INI
+    } 
 
+    /** Mengubah harga jual produk. */
     public void setHargaJual(double hargaJual) {
         this.hargaJual = hargaJual;
     }
 
+     /** Mengubah jumlah stok produk. */
     public void setStok(int stok) {
         this.stok = stok;
     }
@@ -64,9 +75,15 @@ public abstract class ProdukElektronik {
         this.deskripsi = deskripsi;
     }
 
+    /**
+     * Mengurangi stok produk berdasarkan jumlah yang terjual.
+     */
     public void reduceStok(int qty) {
         this.stok = this.stok - qty;
     }
 
+    /**
+     * Menampilkan spesifikasi detail produk (harus diimplementasikan oleh subclass).
+     */
     public abstract void displaySpesifikasi();
 }
