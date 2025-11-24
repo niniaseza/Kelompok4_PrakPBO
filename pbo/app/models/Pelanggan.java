4b7345b7992e6bcf6f3e0e51dd4e7902f6333671
all <t.al.jumanatul.ali53@gmail.com>
2025-11-25 00:09:16 +0700

    Add Pelanggan class with transaction history

--- pbo/app/models/Pelanggan.java
+++ pbo/app/models/Pelanggan.java
@@ -1,1 +1,31 @@
 
+package app.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pelanggan extends User {
+    private String alamat;
+    private String noTelepon;
+    private List<Transaksi> historyTransaksi;
+
+    public Pelanggan(String userID, String username, String password, String nama, String alamat, String noTelepon) {
+        super(userID, username, password, nama);
+        this.alamat = alamat;
+        this.noTelepon = noTelepon;
+        this.historyTransaksi = new ArrayList<>();
+    }
+
+    public String getAlamat() { return alamat; }
+    public String getNoTelepon() { return noTelepon; }
+    public List<Transaksi> getHistoryTransaksi() { return historyTransaksi; }
+
+    public void addToHistory(Transaksi t) { this.historyTransaksi.add(t); }
+
+    public String getId() {
+    return super.getUserID();
+}
+
+    @Override
+    public String getRole() { return "PELANGGAN"; }
+}
