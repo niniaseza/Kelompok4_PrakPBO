7b1d15891f574fe071f2fc5cc9792193a316c777
Azira Kania <azira24@mhs.usk.ac.id>
2025-11-24 23:56:05 +0700

    Update Admin.java

--- pbo/app/models/Admin.java
+++ pbo/app/models/Admin.java
@@ -1,1 +1,62 @@
+package app.models;
 
+import java.util.List;
+import app.models.ProdukElektronik;
+import app.models.Transaksi;
+import app.DataStore;
+
+/**
+ * Kelas Admin mewakili pengguna dengan hak akses tinggi.
+ * Admin dapat mengelola produk dan transaksi dalam sistem.
+ * Menggunakan konsep inheritance dari User.
+ */
+
+public class Admin extends User {
+/**
+     * Membuat objek Admin baru.
+     */
+    public Admin(String userID, String username, String password, String nama) {
+        super(userID, username, password, nama);
+    }
+
+    public String getId() {
+    return super.getUserID();
+}
+
+    @Override
+    public String getRole() { return "ADMIN"; }
+
+     /** Menambah produk ke sistem. */
+    public void tambahProduk(ProdukElektronik p) {
+        DataStore.getListProduk().add(p);
+    }
+
+    /**
+     * Menghapus produk berdasarkan ID.
+     *
+     * @return true jika berhasil dihapus
+     */
+    public boolean hapusProduk(String idProduk) {
+        return DataStore.getListProduk().removeIf(p -> p.getIdProduk().equals(idProduk));
+    }
+
+    public ProdukElektronik cariProdukById(String id) {
+        return DataStore.getListProduk().stream().filter(p -> p.getIdProduk().equals(id)).findFirst().orElse(null);
+    }
+
+    public List<Transaksi> lihatTransaksi() {
+        return DataStore.getListTransaksi();
+    }
+
+    public boolean terimaTransaksi(String idTransaksi) {
+        for (Transaksi t : DataStore.getListTransaksi()) {
+            if (t.getIdTransaksi().equals(idTransaksi) && t.getStatus().equals("PENDING")) {
+                t.setStatus("DITERIMA");
+                // kurangi stok setiap item
+                t.reduceStock();
+                return true;
+            }
+        }
+        return false;
+    }
+}
